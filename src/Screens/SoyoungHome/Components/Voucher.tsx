c22ed449489ea8d1835c93cfee3a3828cd48ff14
Tuan Luong <tuanluong.it@gmail.com>
2023-11-25 13:09:28 +0700

    fix service details

--- src/Screens/SoyoungHome/Components/Voucher.tsx
+++ src/Screens/SoyoungHome/Components/Voucher.tsx
@@ -1,14 +1,12 @@
-import React, { memo, useMemo } from "react";
+import React, { useMemo } from "react";
 import { StyleSheet, TouchableOpacity } from "react-native";
 import {
   _heightScale,
   _moderateScale,
   _widthScale,
 } from "../../../Constant/Scale";
-import { navigation } from "../../../../rootNavigation";
 import ScreenKey from "../../../Navigation/ScreenKey";
 import { useNavigate } from "src/Hooks/useNavigation";
-import FastImage from "@Components/FastImage";
 import useConfigFile from "src/Hooks/useConfigFile";
 import { ConfigFileCode } from "@typings/configFile";
 import { first } from "lodash";
@@ -47,7 +45,6 @@ export default Voucher;
 const styles = StyleSheet.create({
   image: {
     width: _widthScale(350),
-    height: _heightScale(100),
     borderRadius: _moderateScale(8),
   },
 });
