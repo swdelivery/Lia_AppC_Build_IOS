0df4e0219286ae6642c042ec8d14585f220c8dd5
Macbook <macbook@iMac-cua-Macbook.local>
2024-01-13 08:47:11 +0700

    count unread messages, show in tabbar

--- src/Components/TabbarNew/ChatTabIcon.tsx
+++ src/Components/TabbarNew/ChatTabIcon.tsx
@@ -1,15 +1,59 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useMemo } from "react";
 import { StyleSheet } from "react-native";
 
-import IconIChat from "../../SGV/i_chat.svg";
 import IconAChat from "../../SGV/a_chat.svg";
+import IconIChat from "../../SGV/i_chat.svg";
 
-import { _moderateScale } from "../../Constant/Scale";
-import { BASE_COLOR } from "@Constant/Color";
+import { BASE_COLOR, RED, WHITE } from "@Constant/Color";
+import {
+  getPartnerConversations
+} from "@Redux/chat/actions";
+import { getPartnerConversationsState } from "@Redux/chat/selectors";
+import useListFilter from "src/Hooks/useListFilter";
+import Column from "@Components/Column";
+import Text from "@Components/Text";
+import { styleElement } from "@Constant/StyleElement";
 
 const ChatTabIcon = memo((props) => {
+  const { data, refreshData } = useListFilter(
+    getPartnerConversationsState,
+    getPartnerConversations,
+  );
+
+  useEffect(() => {
+    refreshData();
+  }, []);
+
+  const countUnReadMessage = useMemo(() => {
+    let count = data?.reduce((total, item) => {
+      return (total += item?.unreadCount ? item?.unreadCount : 0);
+    }, 0)
+    return count
+  }, [data])
+
   return (
     <>
+      {
+        countUnReadMessage ?
+          <Column
+            style={styleElement.centerChild}
+            zIndex={1}
+            left={8 * 2}
+            position="absolute"
+            borderRadius={8 * 2 / 2}
+            backgroundColor={RED}
+            height={8 * 2}
+            width={8 * 2}>
+            <Text
+              size={12}
+              weight="bold"
+              color={WHITE} style={{ bottom: 0 }}>
+              {countUnReadMessage}
+            </Text>
+          </Column>
+          : <></>
+      }
+
       {props?.focused ? (
         <IconAChat width={24} height={24} color={BASE_COLOR} />
       ) : (
